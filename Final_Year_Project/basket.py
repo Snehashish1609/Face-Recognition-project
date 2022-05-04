3f6de389217c3faba394a1307ac473d7507779cd
Snehashish Lahiri <lahiri.sneh@gmail.com>
2022-05-04 16:51:35 +0530

    Added feature to retrieve product info after scanning rfid tag  --stable build

--- Final_Year_Project/basket.py
+++ Final_Year_Project/basket.py
@@ -1,6 +1,11 @@
 import pyfirmata
 import time
 import serial
+import pymongo
+
+#Database
+client = pymongo.MongoClient('localhost', 27017)
+db = client.project
 
 '''board = pyfirmata.Arduino('COM5')
 print("Arduino communication started!")
@@ -19,7 +24,13 @@ except:
 while True:
     time.sleep(1)
     data = arduino.readline()
-    print(data)
-    #pieces = data.split(" ")
-    #print(*pieces, sep = " ")
+    t = data.decode('UTF-8')
+    t = t.replace('\r', '')
+    t = t.replace('\n', '')
+    t = t.replace(' ', '')
+
+    # Product info
+    print(t)
+    res = db.product.find_one( {"product_id" : t}, {"_id" : 0} )
+    print(res)
     print("-------------------------------")
